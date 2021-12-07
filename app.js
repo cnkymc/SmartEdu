a7ae762e839f34a5aeb758486d8f17345ca3478e
Yunus Can Kuyumcu <65031891+cnkymc@users.noreply.github.com>
2021-12-07 11:11:37 +0300

    SmartEdu
    Lesson 01

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
